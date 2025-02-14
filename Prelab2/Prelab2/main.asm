fb67abe07b6b451529f5916985bf0eb241854758
Adrian-Fernandez99 <fer23381@uvg.edu.gt>
2025-02-13 23:15:20 -0600

    SETUP
    Se inicia el programa con el setup respectivo

--- Prelab2/Prelab2/main.asm
+++ Prelab2/Prelab2/main.asm
@@ -1,12 +1,45 @@
-;
-; Prelab2.asm
-;
-; Created: 2/13/2025 11:07:00 PM
-; Author : adria
-;
+/*
 
+Prelab2.asm
 
-; Replace with your application code
-start:
-    inc r16
-    rjmp start
+Created: 2/13/2025 11:07:00 PM
+Author : Adrián Fernández
+
+Descripción:
+	Se realiza dos contadores binario de 4 bits.
+	El conteo es visto por medio de LEDs en la protoboard.
+	Se usan pushbuttons para el incremento y decrecimiento 
+	de los valores.
+	Por ultimo se suman ambos contadores y se muestran en 4
+	leds aparte, una lad extra para mostrar overflow
+*/
+
+.include "M328PDEF.inc" // Include definitions specific to ATMega328P
+.cseg
+.org 0x0000
+.def COUNTER = R20		// Se define contador
+
+// Configuración de la pila
+	LDI R16, LOW(RAMEND)
+	OUT SPL, R16
+	LDI R16, HIGH(RAMEND)
+	OUT SPH, R16
+
+// Configuración del MCU
+
+// Configurar Prescaler "Principal"
+	LDI R16, (1 << CLKPCE)
+	STS CLKPR, R16			// Habilitar cambio de PRESCALER
+	LDI R16, 0x04
+	STS CLKPR, R16			// Configurar Prescaler a 16 F_cpu = 1MHz
+
+// Inicializar timer0
+	CALL INIT_TMR0
+// Configurar PB5 como salida para usarlo como "LED"
+	SBI DDRB, 5				// Establecer bit PB5 como salida
+	SBI DDRB, 0
+	CBI PORTB, 5			// Obligar a LED a estar "APAGADO" inicialmente
+	CBI PORTB, 0
+// Deshabilitar serial (esto apaga los demas LEDs del Arduino)
+	LDI R16, 0x00
+	STS UCSR0B, R16
