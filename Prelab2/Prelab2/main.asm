fc0c473989feca32c1cfa55061f8f96f89cce20e
Adrian-Fernandez99 <fer23381@uvg.edu.gt>
2025-02-13 23:22:40 -0600

    Main loop
    Se utiliza codigo visto en clase como base

--- Prelab2/Prelab2/main.asm
+++ Prelab2/Prelab2/main.asm
@@ -14,10 +14,10 @@ Descripción:
 	leds aparte, una lad extra para mostrar overflow
 */
 
-.include "M328PDEF.inc" // Include definitions specific to ATMega328P
+.include "M328PDEF.inc"		// Include definitions specific to ATMega328P
 .cseg
 .org 0x0000
-.def COUNTER = R20		// Se define contador
+.def COUNTER = R20			// Se define contador
 
 // Configuración de la pila
 	LDI R16, LOW(RAMEND)
@@ -36,10 +36,36 @@ Descripción:
 // Inicializar timer0
 	CALL INIT_TMR0
 // Configurar PB5 como salida para usarlo como "LED"
-	SBI DDRB, 5				// Establecer bit PB5 como salida
-	SBI DDRB, 0
-	CBI PORTB, 5			// Obligar a LED a estar "APAGADO" inicialmente
-	CBI PORTB, 0
+	SBI DDRD, 5				// Establecer bit PB5 como salida
+	SBI DDRD, 0
+	CBI PORTD, 5			// Obligar a LED a estar "APAGADO" inicialmente
+	CBI PORTD, 0
 // Deshabilitar serial (esto apaga los demas LEDs del Arduino)
 	LDI R16, 0x00
 	STS UCSR0B, R16
+
+// Main loop
+MAIN_LOOP:
+	IN R16, TIFR0			// Leer registro de interrupción de TIMER0
+	SBRS R16, TOV0			// Salta si el bit 0 est "set" (TOV0 bit)
+	RJMP MAIN_LOOP			// Reiniciar loop
+	SBI TIFR0, TOV0			// Limpiar bandera de "overflow"
+	LDI R16, 100
+	OUT TCNT0, R16			// Volver a cargar valor inicial en TCNT0
+	INC COUNTER
+	CPI COUNTER, 10			// R20 = 10 after 100ms
+	BRNE MAIN_LOOP
+	CLR COUNTER
+	SBI PIND, PB5
+	SBI PIND, PB0
+	RJMP MAIN_LOOP
+
+// NON-Interrupt subroutines
+INIT_TMR0:
+LDI R16, (1<<CS01) | (1<<CS00)
+OUT TCCR0B, R16				// Setear prescaler del TIMER 0 a 64
+LDI R16, 100
+OUT TCNT0, R16				// Cargar valor inicial en TCNT0
+RET
+
+// Interrupt routines
