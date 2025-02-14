24223e05438cfc47a816f41c3fce2b077237a631
Adrian-Fernandez99 <fer23381@uvg.edu.gt>
2025-02-14 12:49:33 -0600

    Contador
    Se agrega la función para que el contador funcione

--- Prelab2/Prelab2/main.asm
+++ Prelab2/Prelab2/main.asm
@@ -26,46 +26,51 @@ Descripción:
 	OUT		SPH, R16
 
 // Configuración del MCU
-
 // Configurar Prescaler "Principal"
 	LDI		R16, (1 << CLKPCE)
-	STS		CLKPR, R16			// Habilitar cambio de PRESCALER
+	STS		CLKPR, R16		// Habilitar cambio de PRESCALER
 	LDI		R16, 0x04
-	STS		CLKPR, R16			// Configurar Prescaler a 16 F_cpu = 1MHz
+	STS		CLKPR, R16		// Configurar Prescaler a 16 F_cpu = 1MHz
 
 // Inicializar timer0
 	CALL	INIT_TMR0
 // Configurar PB5 como salida para usarlo como "LED"
-	SBI		DDRD, 5				// Establecer bit PB5 como salida
-	SBI		DDRD, 0
-	CBI		PORTD, 5			// Obligar a LED a estar "APAGADO" inicialmente
-	CBI		PORTD, 0
+	SBI		DDRC, 5			// Establecer bit PB5 como salida
+	SBI		DDRC, 0
+	CBI		PORTC, 5		// Obligar a LED a estar "APAGADO" inicialmente
+	CBI		PORTC, 0
 // Deshabilitar serial (esto apaga los demas LEDs del Arduino)
 	LDI		R16, 0x00
 	STS		UCSR0B, R16
 
 // Main loop
 MAIN_LOOP:
-	IN		R16, TIFR0			// Leer registro de interrupción de TIMER0
-	SBRS	R16, TOV0			// Salta si el bit 0 est "set" (TOV0 bit)
-	RJMP	MAIN_LOOP			// Reiniciar loop
-	SBI		TIFR0, TOV0			// Limpiar bandera de "overflow"
+	IN		R16, TIFR0		// Leer registro de interrupción de TIMER0
+	SBRS	R16, TOV0		// Salta si el bit 0 est "set" (TOV0 bit)
+	RJMP	MAIN_LOOP		// Reiniciar loop
+	SBI		TIFR0, TOV0		// Limpiar bandera de "overflow"
 	LDI		R16, 131
-	OUT		TCNT0, R16			// Volver a cargar valor inicial en TCNT0
+	OUT		TCNT0, R16		// Volver a cargar valor inicial en TCNT0
 	INC		COUNTER
-	CPI		COUNTER, 125			// R20 = 10 after 100ms
+	CPI		COUNTER, 125	// Se necesitan hacer 125 overflows para 100ms
 	BRNE	MAIN_LOOP
-	CLR		COUNTER
-	SBI		PIND, PB5
-	SBI		PIND, PB0
-	RJMP	MAIN_LOOP
+	CLR		COUNTER			// Se reinicia el conteo de overflows
+	CALL	SUMA			// Se llama al incremento del contador
+	OUT		PORTC, R19		// Sale la señal
+	RJMP	MAIN_LOOP		// Regresa al main loop
 
 // NON-Interrupt subroutines
 INIT_TMR0:
 	LDI		R16, (1<<CS01)
-	OUT		TCCR0B, R16			// Setear prescaler del TIMER 0 a 8
+	OUT		TCCR0B, R16		// Setear prescaler del TIMER 0 a 8
 	LDI		R16, 131
-	OUT		TCNT0, R16			// Cargar valor inicial en TCNT0
+	OUT		TCNT0, R16		// Cargar valor inicial en TCNT0
+	RET
+
+SUMA:						// Función para el incremento del primer contador
+	INC		R19				// Se incrementa el valor
+	SBRC	R19, 4			// Se observa si tiene más de 4 bits
+	LDI		R19, 0x00		// En ese caso es overflow y debe regresar a 0
 	RET
 
 // Interrupt routines
